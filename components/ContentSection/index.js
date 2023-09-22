559a7ca1b1cab812d833fad2e2673871697d3cab
Ty Todd <swagginty@gmail.com>
2023-09-22 16:12:08 -0400

    changed markdown to page

--- components/ContentSection/index.js
+++ components/ContentSection/index.js
@@ -2,6 +2,56 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+// import Image from "next/image";
+import PropTypes from "prop-types";
+import remarkGfm from "remark-gfm";
+
+const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired,
+};
+
+function textToStyle(inputString) {
+  if (!inputString.includes(",")) {
+    return { style: {}, text: inputString };
+  }
+  // Split the inputString by the first comma (,) to separate width and height
+  const [widthHeightPart, text] = inputString.split(" ");
+
+  // Split the widthHeightPart by space to get width and height as separate strings
+  const [widthStr, heightStr] = widthHeightPart.split(",");
+
+  // Convert width and height to numbers
+  const width = widthStr;
+  const height = heightStr;
+
+  // Return an object with the extracted values
+  let result = { style: {} };
+  if (width) {
+    result.style.width = !isNaN(parseInt(width)) ? parseInt(width) : width;
+  }
+  if (height) {
+    result.style.height = !isNaN(parseInt(height)) ? parseInt(height) : height;
+  }
+  if (text) {
+    result.text = text;
+  }
+  console.log(result);
+  return result;
+}
 
 const CodeBlock = {
   code({ node, inline, className, children, ...props }) {
@@ -21,11 +71,32 @@ const CodeBlock = {
       </code>
     );
   },
+
+  img({ alt, src, title }) {
+    const style = textToStyle(alt).style;
+    if (alt.includes("youtube")) {
+      return <YoutubeEmbed embedId={src} />;
+    } else {
+      return (
+        <img
+          alt={textToStyle(alt).text}
+          src={src}
+          title={title}
+          style={style}
+        />
+      );
+    }
+  },
 };
 
 const ContentSection = ({ content }) => {
   return (
-    <ReactMarkdown components={CodeBlock} className="markdown-class">
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={CodeBlock}
+      className="markdown-class"
+      skipHtml={true}
+    >
       {content}
     </ReactMarkdown>
   );
